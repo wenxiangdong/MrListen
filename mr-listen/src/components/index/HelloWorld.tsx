88aec20c197017552e05d3b0dc22202c5fc50928
eric <wenxiangdong@outlook.com>
2019-04-18 22:49:46 +0800

    前端初始化

---
+++ mr-listen/src/components/index/HelloWorld.tsx
@@ -0,0 +1,8 @@
+import { Component } from "@tarojs/taro";
+import { Text } from "@tarojs/components";
+
+export default class HelloWorld extends Component {
+    render() {
+        return (<Text>Hello world!</Text>)
+    }
+}
