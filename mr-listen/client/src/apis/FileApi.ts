e3f5f671b6cb3066c3c1b53f944d5e5aa5fefccf
cpppx <chen_st@foxmail.com>
2019-05-08 11:46:30 +0800

    更新 FileApi

--- mr-listen/client/src/apis/FileApi.ts
+++ mr-listen/client/src/apis/FileApi.ts
@@ -27,8 +27,8 @@ export class FileApi implements IFileApi {
       Taro.showLoading({title: "上传文件中..."});
       let res = await Taro.cloud.uploadFile({
         //@ts-ignore
-        cloudPath: `${cloudPath}/${new Date().getTime()}${Math.random()}`,
-        filePath: filePath
+        cloudPath,
+        filePath
       }) as UploadFileResult;
       return res.fileID;
     } catch (e) {
