b97eaa4e58e8822b4649401e942e0a1addfd0227
eric <wenxiangdong@outlook.com>
2019-05-22 20:07:13 +0800

    流星背景

--- mr-listen/client/src/components/DynamicBackground/DynamicBackgroundFactory.tsx
+++ mr-listen/client/src/components/DynamicBackground/DynamicBackgroundFactory.tsx
@@ -3,6 +3,7 @@ import {View} from "@tarojs/components";
 import ColorStripe from "./ColorStripe/ColorStripe";
 import UserConfig, {IUserConfig} from "../../utils/user-config";
 import DynamicBubbles from "./Bubbles/DynamicBubbles";
+import ShootingStar from "./ShootingStar/ShootingStar";
 
 export default class DynamicBackgroundFactory extends Taro.Component {
 
@@ -33,6 +34,10 @@ export default class DynamicBackgroundFactory extends Taro.Component {
       }
       case "bubbles": {
         result = <DynamicBubbles/>;
+        break;
+      }
+      case "shooting-start": {
+        result = <ShootingStar/>
         break;
       }
       case "default": {
---
+++ mr-listen/client/src/components/DynamicBackground/ShootingStar/ShootingStar.tsx
@@ -0,0 +1,19 @@
+import Taro from "@tarojs/taro";
+import {View} from "@tarojs/components";
+
+
+export default class ShootingStar extends Taro.Component {
+  render(): any {
+    return (
+      <View className="wrapper">
+        <View className="night">
+          <View className="star" />
+          <View className="star" />
+          <View className="star"/>
+          <View className="star"/>
+          <View className="star"/>
+        </View>
+      </View>
+    );
+  }
+}
